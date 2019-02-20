fdd984fd431b577eece348b61ac3a4e45660ae6d
Philip Porter <philaporter@gmail.com>
2019-02-19 21:46:15 -0700

    added client code for sending in requests based on the client/config.json

--- client/main.go
+++ client/main.go
@@ -1,7 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/valyala/fasthttp"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	MethodPost      = "POST"
+	ContentTypeJson = "application/json"
+)
+
+type JsonRequest struct {
+	Status string `json:"status"`
+	Id     string `json:"id"`
+	Count  int    `json:"count"`
+}
+
+type ClientConfig struct {
+	Host         string `json:"host"`
+	Port         int    `json:"port"`
+	RequestCount int    `json:"request_count"`
+	Uri          string `json:"uri"`
+}
+
+func loadConfig() ClientConfig {
+	jsonFile, err := os.Open("client/config.json")
+	if err != nil {
+		fmt.Println("Failed to open client/config.json ", err)
+		fmt.Println("Exiting...")
+		os.Exit(1)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var jsonConfig ClientConfig
+	err = json.Unmarshal(byteValue, &jsonConfig)
+	if err != nil {
+		fmt.Println("Failed to unmarshal the JSON defined in client/config.json", err)
+		fmt.Println("Exiting...")
+		os.Exit(1)
+	}
+	return jsonConfig
+}
 
 func main() {
-	fmt.Println("hey")
+	clientConfig := loadConfig()
+	request := fasthttp.AcquireRequest()
+	response := fasthttp.AcquireResponse()
+	client := fasthttp.Client{}
+
+	// Send the configured number of requests
+	for i := 0; i < clientConfig.RequestCount; i++ {
+
+		// Build the json request body
+		reqA := &JsonRequest{
+			Status: "OK",
+			Id:     fmt.Sprintf("%d", i+1) + "-EIEIO",
+			Count:  i + 1,
+		}
+		reqB, err := json.Marshal(reqA)
+		if err != nil {
+			fmt.Println("Error marshaling the json request body: ", err)
+			fmt.Println("Exiting...")
+			os.Exit(1)
+		}
+
+		// Build the request
+		request.Header.SetHost(fmt.Sprintf("%s:%d", clientConfig.Host, clientConfig.Port))
+		request.SetBody(reqB)
+		request.SetRequestURI(clientConfig.Uri)
+		request.Header.SetMethodBytes([]byte(MethodPost))
+		request.Header.SetContentType(ContentTypeJson)
+
+		err = client.Do(request, response)
+		if err != nil {
+			fmt.Println("Error sending request ", err)
+			fmt.Println("Exiting...")
+			os.Exit(1)
+		} else {
+			fmt.Println("Successfully sent ", string(request.Body()))
+		}
+	}
+
 }
--- server/main.go
+++ server/main.go
@@ -8,25 +8,24 @@ import (
 	"os"
 )
 
-type JsonConfig struct {
-	Host         string `json:"host"`
-	Port         int    `json:"port"`
-	RequestCount int    `json:"request_count"`
+type ServerConfig struct {
+	Host string `json:"host"`
+	Port int    `json:"port"`
 }
 
-func loadConfig() JsonConfig {
+func loadConfig() ServerConfig {
 	jsonFile, err := os.Open("server/config.json")
 	if err != nil {
 		fmt.Println("Failed to open server/config.json ", err)
 	}
 	defer jsonFile.Close()
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-	var jsonConfig JsonConfig
-	err = json.Unmarshal(byteValue, &jsonConfig)
+	var serverConfig ServerConfig
+	err = json.Unmarshal(byteValue, &serverConfig)
 	if err != nil {
 		fmt.Println("Failed to unmarshal the JSON defined in server/config.json", err)
 	}
-	return jsonConfig
+	return serverConfig
 }
 
 func handler(ctx *fasthttp.RequestCtx) {
@@ -35,14 +34,27 @@ func handler(ctx *fasthttp.RequestCtx) {
 	case "/test":
 		method := string(ctx.Method())
 		if method != "GET" && method != "DELETE" {
-			fmt.Println(method)
+			fmt.Println("Body received: ", string(ctx.Request.Body()))
 			fmt.Fprint(ctx, "Body received: ", ctx.Request.Body())
+			//fmt.Println("Total number of requests made on this connection: ", ctx.ConnRequestNum())
 		} else {
+			fmt.Println(ctx, "My dog is cuter than yours. Runs faster, too.")
 			fmt.Fprint(ctx, "My dog is cuter than yours. Runs faster, too.")
 		}
 	case "/connection/stats":
 		fmt.Fprint(ctx, "Total number of requests made on this connection: ", ctx.ConnRequestNum())
 	default:
+		fmt.Println("    **   ****     ** \n")
+		fmt.Println("   */*  *///**   */* \n")
+		fmt.Println("  * /* /*  */*  * /* \n")
+		fmt.Println(" ******/* * /* ******\n")
+		fmt.Println("/////* /**  /*/////* \n")
+		fmt.Println("    /* /*   /*    /* \n")
+		fmt.Println("    /* / ****     /* \n")
+		fmt.Println("    /   ////      /  \n\n")
+		fmt.Println("Send a GET to /connection/stats for a total number of requests for an active connection.\n")
+		fmt.Println("Send a GET or DELETE to /test for the truth of my dog versus your dog.\n")
+		fmt.Println("Send a PUT or POST to /test for a print out of the request body sent.\n")
 		fmt.Fprint(ctx, "    **   ****     ** \n")
 		fmt.Fprint(ctx, "   */*  *///**   */* \n")
 		fmt.Fprint(ctx, "  * /* /*  */*  * /* \n")
@@ -61,7 +73,6 @@ func handler(ctx *fasthttp.RequestCtx) {
 func main() {
 	jsonConfig := loadConfig()
 	fmt.Println("Starting server, listening on " + fmt.Sprintf("%s:%d", jsonConfig.Host, jsonConfig.Port))
-
 	err := fasthttp.ListenAndServe(fmt.Sprintf("%s:%d", jsonConfig.Host, jsonConfig.Port), handler)
 	if err != nil {
 		fmt.Print("The server is tired of serving. Exiting...")
